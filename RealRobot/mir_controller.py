42c59da882926eaa4467a3aeb766a92499c88aaa
Mathu Jeyanth <mathujeyanth@hotmail.dk>
2019-04-08 14:26:17 +0200

    updated controller script

--- RealRobot/mir_controller.py
+++ RealRobot/mir_controller.py
@@ -37,7 +37,7 @@ def lidarToZones(data):
 
         lidarRange = data[x]
 
-        if lidarRange > 5 or lidarRange < 0.1:
+        if lidarRange > 5 or lidarRange < 0.01:
             hits[x]=1
         else:
             hits[x]=round(((lidarRange-safetyDistances[x])/5),2)
@@ -66,29 +66,29 @@ def path_callback(data):
     global running
     global x_pose
     global y_pose
-
+    global path_length
     running = False
     path_index = 0
     length_a = len(data.poses)
-
+    path_length = length_a
     if length_a > 0:
         #print("New path")
         
         running = True
         glo_path = np.zeros((2,length_a))
-        for x in reversed(range(0,length_a)):
+        for x in reversed(range(0,length_a)): #reversed(
             glo_path[0,x] = data.poses[x].pose.position.x
             glo_path[1,x] = data.poses[x].pose.position.y
         #np.flip(glo_path,0)
         #print(glo_path)
-        index = 0
-        while glo_path[0,0] == glo_path[0,index] or glo_path[1,0] == glo_path[1,index]:
-            index +=1
+        #index = 0
+        #while glo_path[0,0] == glo_path[0,index] or glo_path[1,0] == glo_path[1,index]:
+        #    index +=1
         #path_index = index
-        v1 = np.array([glo_path[0,0],glo_path[1,0]])
-        v2 = np.array([glo_path[0,index],glo_path[1,index]])
-        v3 = v1 - v2
-        simple = np.array([0,1])
+        #v1 = np.array([glo_path[0,0],glo_path[1,0]])
+        #v2 = np.array([glo_path[0,index],glo_path[1,index]])
+        #v3 = v1 - v2
+        #simple = np.array([0,1])
 
 def robot_pose_callback(data):
     #print(data.position)
@@ -122,26 +122,26 @@ def update_rob_pos():
     global y_pose
     global angle
     global d1
+    global path_length
 
     rob_x = x_pose
     rob_y = y_pose
 
-    if (glo_path.size/2) != 0 and (glo_path.size/2) > path_index+2:
+    if path_length != 0 and path_length > path_index+2:
         #print(path_index)
         d1 = sqrt((glo_path[0,path_index]       -rob_x)**2+(glo_path[1,path_index]      -rob_y)**2)
         d2 = sqrt((glo_path[0,(path_index+1)]   -rob_x)**2+(glo_path[1,(path_index+1)]  -rob_y)**2)
 
-        while d1 >= d2 and (glo_path.size/2) > path_index+2:
+        while d1 >= d2 and path_length > path_index+2:
             path_index = path_index + 1
             d1 = d2
             d2 = sqrt((glo_path[0,path_index+1]-rob_x)**2+(glo_path[1,path_index+1]-rob_y)**2)
 
-        if (glo_path.size/2) > path_index+41:
+        if path_length > path_index+41:
             v1 = np.array([glo_path[0,path_index+40],glo_path[1,path_index+40]])
         else:
-            v1 = np.array([glo_path[0,len(glo_path)-1],glo_path[1,len(glo_path)-1]])
-            d = sqrt((glo_path[0,len(glo_path)-1]       -rob_x)**2+(glo_path[1,len(glo_path)-1]      -rob_y)**2)
-
+            v1 = np.array([glo_path[0,path_length-1],glo_path[1,path_length-1]])
+            d = sqrt((glo_path[0,path_length-1]       -rob_x)**2+(glo_path[1,path_length-1]      -rob_y)**2)
             if d < 1:
                 running = False
 
@@ -152,10 +152,22 @@ def update_rob_pos():
 
 
 def move():
+    global linear_speed
+    global angular_speed
+    global b_lidar
+    global f_lidar
+    global running
+    global robot_angle
+    global path_index
+    global glo_path
+    global x_pose
+    global y_pose
+    global angle
+    global d1
     # output to file
     #f = open('recording.txt','w')
     #Tensorflow stuff
-    pred=predictor.Predictions("MiR_Robot_LBrain.pb")
+    pred=predictor.Predictions("MiR_Robot_LBrain3.pb")
     # calc safety distances
     global safetyDistances
     vinkelB = 30.83
@@ -174,10 +186,10 @@ def move():
     rospy.init_node('MiR_controller', anonymous=True)
 
     rospos_sub = rospy.Subscriber("/robot_pose",Pose,robot_pose_callback)
-    b_laserScan_sub = rospy.Subscriber("/b_scan", LaserScan, b_lidar_callback)  ## len(data.ranges)
-    f_laserScan_sub = rospy.Subscriber("/f_scan", LaserScan, f_lidar_callback)
+    b_laserScan_sub = rospy.Subscriber("/b_raw_scan", LaserScan, b_lidar_callback)  ## len(data.ranges)
+    f_laserScan_sub = rospy.Subscriber("/f_raw_scan", LaserScan, f_lidar_callback)
 
-    rate = rospy.Rate(5)
+    rate = rospy.Rate(2)
     rate.sleep()
 
     velocity_publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
@@ -197,14 +209,14 @@ def move():
     time_frame_2 = np.zeros(20)
     time_frame_2.tolist()
 
-    time_frame_3 = np.zeros(20)
-    time_frame_3.tolist()
+    # time_frame_3 = np.zeros(20)
+    # time_frame_3.tolist()
 
-    time_frame_4 = np.zeros(20)
-    time_frame_4.tolist()
+    # time_frame_4 = np.zeros(20)
+    # time_frame_4.tolist()
     
-    time_frame_5 = np.zeros(20)
-    time_frame_5.tolist()
+    # time_frame_5 = np.zeros(20)
+    # time_frame_5.tolist()
 
     key = bool(input("Start?"))
 
@@ -213,6 +225,7 @@ def move():
         #print(f_lidar)
         #print(b_lidar)
         
+
         if running:
             update_rob_pos()
             #print("Robot angle and angle to position")
@@ -223,8 +236,9 @@ def move():
             elif angle_dif < -pi:
                 angle_dif = angle_dif + 2 * pi
 
+            
 
-            input_array = ([round(linear_speed,2)*2,round(angular_speed,2)*2, round((angle_dif/pi),2),round(d1/maxDeviation,2)])            
+            input_array = ([round(linear_speed,2),round(angular_speed,2), round((angle_dif/pi),2),round(d1/maxDeviation,2)])            
             input_array.extend(f_lidar.tolist())
             input_array.extend(b_lidar.tolist())
 
@@ -232,34 +246,38 @@ def move():
             #f.write(our_string)
             #f.write('\n')
 
-            time_frame_5 = time_frame_4
-            time_frame_4 = time_frame_3
-            time_frame_3 = time_frame_2
+            #time_frame_5 = time_frame_4
+            #time_frame_4 = time_frame_3
+            #time_frame_3 = time_frame_2
             time_frame_2 = time_frame_1
             time_frame_1 = input_array
             
             #input_tensor.clear()
 
-            input_tensor = np.array([time_frame_1,time_frame_2,time_frame_3,time_frame_4,time_frame_5])
+            input_tensor = np.array([time_frame_1,time_frame_2]) # ,time_frame_3,time_frame_4,time_frame_5
             input_tensor = input_tensor.flatten()
 
             output_tensor = np.array([0,0])
             eps = np.array([0.2,0.2])
             output_tensor = pred.getPrediction([eps], [input_tensor])
 
-            linear_vel = output_tensor[0][1] * 0.5
+            linear_vel = output_tensor[0][1] * 0.8
             if linear_vel < 0:
                 linear_vel = linear_vel * (1/3)
-            angular_vel = output_tensor[0][0] * 0.5
+            angular_vel = output_tensor[0][0]
             
 
-            if abs(angular_vel) < 0.05:
-                angular_vel = 0
+            #if abs(angular_vel) < 0.1:
+            #    angular_vel = 0
+
+            if sqrt(linear_vel**2+angular_vel**2) > 1 and linear_vel > 0:
+                linear_vel = sqrt(1-(angular_vel**2))
 
             print("Input array")
             print(input_array)
             #print("Angle dif")
             #print(angle_dif)
+
 
             vel_msg.linear.x = linear_vel
             vel_msg.linear.y = 0
